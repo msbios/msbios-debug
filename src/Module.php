d5ffb15d91b9d8241f2ecc779c18088af80e994f
Judzhin Miles <info@msbios.com>
2018-08-19 12:23:49 +0300

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -17,7 +17,7 @@ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 class Module implements ModuleInterface, AutoloaderProviderInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.3';
+    const VERSION = '1.0.4';
 
     /**
      * Returns configuration to merge with application configuration
