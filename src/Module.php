e468cf8489c09f7aa1e2c43077a7eea084e29993
Judzhin Miles <info@msbios.com>
2019-11-01 22:38:46 +0200

    Lazy Composer MSBios Commit Script

--- src/Module.php
+++ src/Module.php
@@ -12,7 +12,7 @@ namespace MSBios\Debug;
 class Module extends \MSBios\Module
 {
     /** @const VERSION */
-    const VERSION = '1.0.5';
+    const VERSION = '1.0.6';
 
     /**
      * @inheritdoc
